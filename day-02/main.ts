5842687e45af96091b502bd2b57448a7695b6e34
Michael Gallagher <mcgallag@gmail.com>
2020-12-03 21:08:04 -0500

    Day 02 - part 2

--- day-02/main.ts
+++ day-02/main.ts
@@ -8,26 +8,65 @@ const input_index = 1;
 fs.readFile("./day-02/" + input[input_index], "utf8")
   .then(txt => {
     let passwords = txt.split('\n');
-    let valid = getValidPasswords(passwords);
-    console.log("Number of valid passwords: " + valid.length);
+    let valid = getValidSledPasswords(passwords);
+    console.log("Number of valid sled passwords: " + valid.length);
+
+    valid = getValidTobogganPasswords(passwords);
+    console.log("Number of valid toboggan passwords: " + valid.length);
   });
 
 /**
- * Parses each password and policy. Returns an array of passwords
- * that are valid for the policy. 
+ * Parses line from puzzle input into a useable object
+ * @param str line from puzzle input file
+ */
+function parseLine(str: string): {
+  low: number,
+  high: number,
+  password: string,
+  char: string
+} {
+  // 5-14 l: lmlmnrzlglflll
+  let [minmax, char, password] = str.split(' ');
+  let [low, high] = minmax.split("-").map(num => parseInt(num));
+  char = char[0];
+
+  return {
+    low, high, password, char
+  };
+}
+
+/**
+ * Parses each password and sled rental policy. Returns an array of passwords
+ * that are valid for the policy.
  * @param arr list of passwords and policies to check against
  */
-function getValidPasswords(arr: string[]): string[] {
+function getValidSledPasswords(arr: string[]): string[] {
   let valid: string[] = arr.filter(line => {
-    // 5-14 l: lmlmnrzlglflll
-    let [minmax, char, password] = line.split(' ');
-    let [min, max] = minmax.split("-").map(str => parseInt(str));
-    char = char[0];
+    let o = parseLine(line);
 
-    let passwordLetters = password.split("");
-    let charMatches = passwordLetters.filter(letter => (letter == char));
+    let passwordLetters = o.password.split("");
+    let charMatches = passwordLetters.filter(letter => (letter == o.char));
 
-    return (charMatches.length >= min) && (charMatches.length <= max);
+    return (charMatches.length >= o.low) && (charMatches.length <= o.high);
+  });
+
+  return valid;
+}
+
+/**
+ * Parses each password and toboggan rental policy. Returns an array of passwords
+ * that are valid for the policy.
+ * @param arr list of passwords and policies to check against
+ */
+function getValidTobogganPasswords(arr: string[]): string[] {
+  let valid: string[] = arr.filter(line => {
+    let o = parseLine(line);
+    let ch1 = o.password[o.low - 1];
+    let ch2 = o.password[o.high - 1];
+    return (
+      // XOR
+      (ch1 == o.char || ch2 == o.char) && !(ch1 == o.char && ch2 == o.char)
+    );
   });
 
   return valid;
